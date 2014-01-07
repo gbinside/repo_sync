1587d1d649ebfc094b769366f4582e7bd5a78f25
Roberto Gambuzzi <gbinside@gmail.com>
2014-01-07 17:25:36 +0100

    test bitbucket

--- __main__.py
+++ __main__.py
@@ -17,10 +17,31 @@
 from pprint import pprint
 import ConfigParser
 import subprocess
-import urllib
+import urllib2
+import base64
 import json
 import sys
 import os
+
+#http://stackoverflow.com/questions/2407126/python-urllib2-basic-auth-problem
+
+# request = urllib2.Request("http://api.foursquare.com/v1/user")
+# base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+# request.add_header("Authorization", "Basic %s" % base64string)
+# result = urllib2.urlopen(request)
+
+
+def do_work(working_dir, ssh_url, name):
+    print "do_work", working_dir, ssh_url, name
+    os.chdir(working_dir)
+    ret = subprocess.call(['git', 'clone', ssh_url])
+    if ret == 128:  # already exists
+        print "\nGIT PULL\n"
+        os.chdir(os.path.join(working_dir, name))
+        ret = [subprocess.call(['git', 'fetch', '--all'])]
+        ret.append(subprocess.call(['git', 'pull']))
+    return ret
+
 
 if __name__ == "__main__":
     #clone all git repos of a user
@@ -34,23 +55,34 @@ if __name__ == "__main__":
         pprint(config.items(k))
 
     for service in config.sections():
-        if service == 'github':
-            for k, v in config.items(service):
-                if k.startswith('user'):
-                    user, passwd = v.split(':')
-                    working_dir = os.path.join(os.path.dirname(sys.argv[0]), '../%s/%s' % (service, user))
-                    try:
-                        os.makedirs(working_dir)
-                    except:
-                        pass
-                    _json = urllib.urlopen("https://api.github.com/users/%s/repos" % user)
+        for k, v in config.items(service):
+            if k.startswith('user'):
+                user, passwd = v.split(':')
+                auth_key = base64.encodestring(v).replace('\n', '')
+                working_dir = os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), service, user)
+                try:
+                    os.makedirs(working_dir)
+                except:
+                    pass
+
+                if service == 'github':
+                    #_json = urllib.urlopen("https://api.github.com/users/%s/repos" % user)
+                    request = urllib2.Request("https://api.github.com/user/repos")
+                    request.add_header("Authorization", "Basic %s" % auth_key)
+                    _json = urllib2.urlopen(request)
                     for row in json.load(_json):
                         ssh_url = row['ssh_url']
-                        print ssh_url,
-                        os.chdir(working_dir)
-                        ret = subprocess.call(['git', 'clone', ssh_url])
-                        if ret == 128: #already exists
-                            print "\nGIT PULL\n"
-                            os.chdir(os.path.join(working_dir, row['name']))
-                            print subprocess.call(['git', 'fetch', '--all'])
-                            print subprocess.call(['git', 'pull'])
+                        do_work(working_dir, ssh_url, row['name'])
+
+                elif service == 'bitbucket':
+                    request = urllib2.Request("https://bitbucket.org/api/1.0/user/repositories")
+                    request.add_header("Authorization", "Basic %s" % auth_key)
+                    _json = urllib2.urlopen(request)
+                    for row in json.load(_json):
+                        #git clone git@bitbucket.org:
+                        if row['owner'] == user:
+                            #https://gambuzzi@bitbucket.org/webgriffe/drupal_sarchio.git
+                            ssh_url = row["resource_uri"].replace('/1.0/repositories/',
+                                                                  'https://%s@bitbucket.org/' % v) + '.git'
+                            do_work(working_dir, ssh_url, row['name'])
+
